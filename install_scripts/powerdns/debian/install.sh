69e4b92e7cde4bd606bf270c60ad7d8c48bf93ed
root <root@ns3.parking.st>
2020-03-07 22:20:22 +0200

    First steps

--- install_scripts/powerdns/debian/install.sh
+++ install_scripts/powerdns/debian/install.sh
@@ -1,25 +1,60 @@
 #!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+
+die() {
+    # For really rough situations. Don't even log. Just die. Immediately.
+    local -r f_message=${1:-"DIED!"}
+    echo "DYING! ${BASH_SOURCE[1]}: line ${BASH_LINENO[0]}: ${FUNCNAME[1]}: ${f_message}" >&2
+    # shellcheck disable=SC2086
+    exit ${2:-1}
+}
+
+
+BASH_MAJOR_VERSION=$(echo "${BASH_VERSION}" | cut --delimiter='.' --fields=1) \
+    || die "Unable to get bash major version."
+if [[ ${BASH_MAJOR_VERSION} -lt 4 ]]; then
+    die "Shell is too old: bash version 4 or newer is required."
+fi
+
+
+# 'yes'/anything
+declare -r DO_INITIAL_UPDATE='yes'
+
+# 'distrib'/'latest'
+declare -r VERSION_DESIRED_MARIADB='distrib'
+
+
 # get script absolute path
 MY_PATH="`dirname \"$0\"`"
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
 if [ -z "$MY_PATH" ] ; then
 	  exit 1
 fi
+
+
 # upgrade system and install dependencies
 apt-get update && apt-get -y upgrade
 apt-get -y install software-properties-common dirmngr
 apt-get -y install git python-pip
+
 # install and prepare last stable mariadb version
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8
 add-apt-repository 'deb [arch=amd64] http://mariadb.mirror.liquidtelecom.com/repo/10.4/debian buster main'
 apt-get update && apt-get -y install mariadb-server 
+
 # run the secure script to set root password, remove test database and disable remote root user login, you can safely accept the defaults and provide an strong root password when prompted
 mysql_secure_installation
 mysql -u root -p < ${MY_PATH}/sql01.sql # provide previously set password
+
 # install powerdns and configure db parameters
 apt-get -y install pdns-server pdns-backend-mysql
 cp ${MY_PATH}/pdns.local.gmysql.conf /etc/powerdns/pdns.d/
 vi /etc/powerdns/pdns.d/pdns.local.gmysql.conf # db configuration
+
 # install dnsutils for testing, curl and finally PowerDNS-Admin
 apt-get -y install python3-dev dnsutils curl
 apt-get -y install -y default-libmysqlclient-dev python-mysqldb libsasl2-dev libffi-dev libldap2-dev libssl-dev libxml2-dev libxslt1-dev libxmlsec1-dev pkg-config
@@ -39,11 +74,13 @@ vi powerdnsadmin/default_config.py
 export FLASK_APP=powerdnsadmin/__init__.py
 flask db upgrade
 flask db migrate -m "Init DB"
+
 # install/update nodejs, needed to use yarn
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
 yarn install --pure-lockfile
 flask assets build
+
 # create systemd service file and activate it
 mkdir /run/powerdns-admin
 chown pdns:pdns /run/powerdns-admin
