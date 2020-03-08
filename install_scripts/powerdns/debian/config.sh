ba28c15305d9838c72e89b7272512d01cd4b9469
Dmytro Lysytsyn <ldv@kntele.com>
2020-03-08 04:26:04 +0200

    Refining PowerDNS & poweradmin installation sequence.

--- install_scripts/powerdns/debian/config.sh
+++ install_scripts/powerdns/debian/config.sh
@@ -5,34 +5,34 @@
 # WARNING! EXTREMELY DANGEROUS!!! DO NOT TURN IT ON IN PRODUCTION!!!
 # For script's debugging purpose ONLY.
 # 'yes' / anything
-declare -r CLEAR_EXISTING_INSTALL='yes'
+declare -g -r CLEAR_EXISTING_INSTALL='yes'
 
 # 'yes' / anything
-declare -r DO_INITIAL_UPDATE='yes'
+declare -g -r DO_INITIAL_UPDATE='yes'
 
 # 'distrib' / 'latest' / 'none'
-declare -r VERSION_DESIRED_MARIADB='distrib'
+declare -g -r VERSION_DESIRED_MARIADB='distrib'
 
 # 'yes' / anything
-declare -r INITIATE_PDNS_DB='yes'
+declare -g -r INITIATE_PDNS_DB='yes'
 
 # 'yes' / anything
-declare -r INITIATE_PDA_DB='yes'
+declare -g -r INITIATE_PDA_DB='yes'
 
-declare -r MYSQL_ADMIN_USER='root'
-declare -r MYSQL_ADMIN_PASSWD='CHANGEMECHANGEMECHANGEME'
+declare -g -r MYSQL_ADMIN_USER='root'
+declare -g -r MYSQL_ADMIN_PASSWD='CHANGEMECHANGEMECHANGEME'
 
-declare -r PDNS_DB_NAME='powerdns'
-declare -r PDNS_DB_HOST='127.0.0.1'
-declare -r PDNS_DB_USER='pdnsuser'
-declare -r PDNS_DB_PASSWD='CHANGEMECHANGEMECHANGEME'
-declare -r PDNS_DB_USERFROM='127.0.0.1'
+declare -g -r PDNS_DB_NAME='powerdns'
+declare -g -r PDNS_DB_HOST='127.0.0.1'
+declare -g -r PDNS_DB_USER='pdnsuser'
+declare -g -r PDNS_DB_PASSWD='CHANGEMECHANGEMECHANGEME'
+declare -g -r PDNS_DB_USERFROM='127.0.0.1'
 
-declare -r PDA_DB_NAME='pdnsadmin'
-declare -r PDA_DB_HOST='127.0.0.1'
-declare -r PDA_DB_USER='pdnsadminuser'
-declare -r PDA_DB_PASSWD='CHANGEMECHANGEMECHANGEME'
-declare -r PDA_DB_USERFROM='127.0.0.1'
+declare -g -r PDA_DB_NAME='pdnsadmin'
+declare -g -r PDA_DB_HOST='127.0.0.1'
+declare -g -r PDA_DB_USER='pdnsadminuser'
+declare -g -r PDA_DB_PASSWD='CHANGEMECHANGEMECHANGEME'
+declare -g -r PDA_DB_USERFROM='127.0.0.1'
 
 
 
